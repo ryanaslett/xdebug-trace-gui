3f9d6de1737a64850d016042ab9e6801f59d3e49
Vitali Yakavenka <vitali@yakavenka.net>
2012-12-18 15:51:48 -0800

    Extract OutputList::BUFFER_LEVELS. Replace unnecessary sprintf() with string concatenation to to get a bit faster output

--- src/XdebugTraceOutputList.php
+++ src/XdebugTraceOutputList.php
@@ -3,8 +3,9 @@
 use \XdebugTraceReader as Reader;
 
 class XdebugTraceOutputList {
+    const BUFFER_LEVELS = 4;
     public function __construct($warnTimeJump = null, $warnMemoryJump = null) {
-        $this->priviousLevel = 0;
+        $this->previousLevel = 0;
         $this->memJump = (float) $warnMemoryJump;
         $this->timeJump = (float) $warnTimeJump;
     }
@@ -12,30 +13,29 @@ class XdebugTraceOutputList {
     public function printLine($data) {
         if (!$data[Reader::POINT]) {
             $executionTime = $memoryUsage = 0;
-            $callInfo = sprintf('<li id="call%d">%s() %s:%d',
-                $data[Reader::ID], $data[Reader::NAME], 
-                $data[Reader::FILENAME], $data[Reader::LINE]);
+            $callInfo = "<li id=\"call{$data[Reader::ID]}\">" .
+                "{$data[Reader::NAME]}() {$data[Reader::FILENAME]}:" .
+                "{$data[Reader::LINE]}";
         } else {
             $executionTime = Reader::getExecutionTime($data);
             $memoryUsage = Reader::getMemoryUsage($data);
             $warning = $executionTime > $this->timeJump
                 || $memoryUsage > $this->memJump;
             $callInfo = sprintf(
-                ' <span class="stat%s">%.3fms / %+.4f Mb</span></li>%s',
+                " <span class=\"stat%s\">%.3fms / %+.4f Mb</span></li>\n",
                 $warning ? " warning" : "",
                 $executionTime * 1000,
-                $memoryUsage / (1024 * 1024),
-                PHP_EOL);
+                $memoryUsage / (1024 * 1024));
         }
 
         if ($data[Reader::LEVEL] > $this->previousLevel) {
-            if ($data[Reader::LEVEL] >= 3) { ob_start(); }
+            if ($data[Reader::LEVEL] >= self::BUFFER_LEVELS) { ob_start(); }
             echo "<ul>\n"; 
         }
         elseif ($data[Reader::LEVEL] < $this->previousLevel) {
             echo "</ul>\n";
-            $dropNestedCalls = $data[Reader::LEVEL] >= 3;
-            $flushNestedCalls = $data[Reader::LEVEL] >= 3 
+            $dropNestedCalls = $data[Reader::LEVEL] >= self::BUFFER_LEVELS;
+            $flushNestedCalls = $data[Reader::LEVEL] >= self::BUFFER_LEVELS 
                 && ($memoryUsage > $memJump
                     || $executionTime > $timeJump);
             if ($flushNestedCalls) {
